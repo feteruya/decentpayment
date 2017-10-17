0b40bd7bce3d0085a07f9f686d3fe85db34f35f8
Marcelo Menezes <gnumarcelo@gmail.com>
2017-10-17 09:38:02 -0200

    remove comments

--- oracle_server/server.js
+++ oracle_server/server.js
@@ -28,8 +28,6 @@ fs.readFile('../build/contracts/OracleI.json', (error, json) => {
   });
 });
 
-// TWILLIO_SID=AC7dc9fa683de804bffa3d6eaf40cc2259 TWILLIO_AUTH_TOKEN=78ba763a661e77d68d0917333d16cde1 CONTRACT_ADDRESS=0x82ce64e45e86ed5b81a234d2004b01f2c526373b TWILLIO_FROM_NUMBER=+14086694750  ETHEREUM_RPC_URL=http://localhost:8545/ node watcher.js
-
 // CONTRACT_ADDRESS=0x1cff0dc17fd2f2a56bab826bb8a7abae85d72692 node watcher.js
 
 
